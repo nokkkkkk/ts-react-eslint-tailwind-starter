7ca84c37af460927a029162a47f1072ebccb9db1
nokkkkkk <guillaume@aventurequatresaisons.com>
2022-01-20 20:08:34 -0500

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const element = React.createElement('h1', { className: 'flex justify-center text-red-600  text-9xl h-40  ' }, 'newInstaGram');
+
+ReactDOM.render(
+  element,
+  document.getElementById('test')
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
